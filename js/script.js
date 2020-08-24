6e735c3fd863c53fcb657de49bc9cbc4f122e10d
Robert B <robertb@lifesciencepublishers.com>
2020-08-24 12:08:49 -0600

    add dynamic pime

--- js/script.js
+++ js/script.js
@@ -28,9 +28,18 @@ function togglePlay(){
 }
 
 // Progress Bar ---------------------------------- //
+function displayTime(time) {
+    const minutes = Math.floor(time / 60);
+    let seconds = Math.floor(time % 60);
+    seconds = seconds > 9 ? seconds : `0${seconds}`;
+    return `${minutes}:${seconds}`;
+}
 
 function updateProgress() {
     progressBar.style.width = `${(video.currentTime / video.duration) * 100}%`
+    currentTime.textContent = `${displayTime(video.currentTime)}/`;
+    duration.textContent = `${displayTime(video.duration)}`;
+
 }
 
 // Volume Controls --------------------------- //
