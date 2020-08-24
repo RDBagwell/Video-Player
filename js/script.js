607e8a0d773b8621b985594acad085d9390031f5
Robert Bagwell <erbagwell@yahoo.com>
2020-08-23 23:41:12 -0600

    add start stop function

--- js/script.js
+++ js/script.js
@@ -1,8 +1,31 @@
-
+const video = document.querySelector('video');
+const progressRange = document.querySelector('.progress-range');
+const progressBar = document.querySelector('.progress-bar');
+const playBtn = document.getElementById('play-btn');
+const volimeIcon = document.getElementById('volume-icon');
+const volumeRange = document.querySelector('.volume-range');
+const volumeBar = document.querySelector('.volume-bar');
+const currentTime = document.querySelector('.time-elapsed');
+const duration = document.querySelector('.time-duration');
+const FullscreenBtn = document.querySelector('.fullscreen');
 
 // Play & Pause ----------------------------------- //
 
+function showPlayIcon() {
+    playBtn.classList.replace('fa-pause', 'fa-play');
+    playBtn.setAttribute('tile', 'play');
+}
 
+function togglePlay(){
+    if(video.paused){
+        video.play();
+        playBtn.classList.replace('fa-play', 'fa-pause');
+        playBtn.setAttribute('tile', 'pause');
+    } else {
+        video.pause();
+        showPlayIcon();
+    }
+}
 
 // Progress Bar ---------------------------------- //
 
@@ -18,4 +41,7 @@
 
 // Fullscreen ------------------------------- //
 
-
+// Event listeners
+playBtn.addEventListener('click', togglePlay);
+video.addEventListener('click', togglePlay);
+video.addEventListener('ended', showPlayIcon);
